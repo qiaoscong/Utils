e58f1fa1a3365df3fcff2807897396299fa83a3f
qiaoscong <2986584919@qq.com>
2018-11-23 08:52:28 +0800

    Initial commit

---
+++ app/src/main/java/text/qiao/com/utils/utils/CollectionUtil.java
@@ -0,0 +1,234 @@
+package text.qiao.com.utils.utils;
+
+import android.text.TextUtils;
+import android.widget.EditText;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * Created by Administrator on 2018/2/5.
+ * 集合判断工具
+ */
+
+public class CollectionUtil {
+    /**
+     * 判断是否为空
+     *
+     * @param collection 集合
+     * @return 空位true, 否则为false
+     */
+    public static <E> boolean isEmpty(Collection<E> collection) {
+        return collection == null || collection.isEmpty();
+    }
+
+    /**
+     * 判断是否为空
+     *
+     * @param map map
+     * @return 空位true, 否则为false
+     */
+    public static <K, V> boolean isEmpty(Map<K, V> map) {
+        return map == null || map.isEmpty();
+    }
+
+    /**
+     * 判断是否包含字符串
+     *
+     * @param arrs 数组
+     * @param val  是否包含的字符串
+     * @return 包含返回true, 否则为false
+     */
+    public static boolean isContains(String[] arrs, String val) {
+        if (arrs == null || TextUtils.isEmpty(val)) {
+            return false;
+        }
+        for (String arrVal : arrs) {
+            if (val.equals(arrVal)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * 检测是否有emoji表情
+     *
+     * @param source
+     * @return
+     */
+    public static boolean containsEmoji(String source) {
+        int len = source.length();
+        for (int i = 0; i < len; i++) {
+            char codePoint = source.charAt(i);
+            if (!isEmojiCharacter(codePoint)) { //如果不能匹配,则该字符是Emoji表情
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     *  是否输入表情
+     * @param codePoint
+     * @return
+     */
+    public static boolean isEmojiCharacter(char codePoint) {
+        return ((codePoint == 0x0) || (codePoint == 0x9) || (codePoint == 0xA) || (codePoint == 0xD) || ((codePoint >= 0x20) && codePoint <= 0xD7FF)) || ((codePoint >= 0xE000) && (codePoint <= 0xFFFD)) || ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF));
+    }
+    /**
+     * 包括空格判断
+     *
+     * @param input
+     * @return
+     */
+    public static boolean containSpace(CharSequence input) {
+        return input.toString().matches("^\\s*$");
+    }
+
+    /**
+     * 判断手机号
+     *
+     * @param number
+     * @return
+     */
+    public static boolean panDuanShouJiHao(String number) {
+        boolean shu = number.matches("^((13[4-9])|(147)|(15[0-2,7-9])|(178)|(18[2-4,7-8]))\\d{8}|(1705)\\d{7}$\n");//移动
+        boolean shu1 = number.matches("^((13[0-2])|(145)|(15[5-6])|(176)|(18[5,6]))\\d{8}|(1709)\\d{7}$\n");//联通
+        boolean shu2 = number.matches("^((133)|(153)|(177)|(18[0,1,9]))\\d{8}$\n");//电信
+        return isChinaPhoneLegal(number);
+    }
+
+    public static boolean isChinaPhoneLegal(String str)
+            throws PatternSyntaxException {
+        String regExp = "^((13[0-9])|(15[^4])|(166)|(17[0-8])|(18[0-9])|(19[8-9])|(147,145))\\d{8}$";
+        Pattern p = Pattern.compile(regExp);
+        Matcher m = p.matcher(str);
+        return m.matches();
+    }
+    public static boolean yanMiMa(String password) {
+        boolean Ispassword = password.matches("^[a-zA-Z0-9]+$");
+        return Ispassword;
+    }
+
+    /**
+     * 纯数字
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isNumeric(String str) {
+        for (int i = str.length(); --i >= 0; ) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 纯字母
+     *
+     * @param fstrData
+     * @return
+     */
+    public static boolean isChar(String fstrData) {
+        char c = fstrData.charAt(0);
+        if (((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /**
+     * 规则1：至少包含大小写字母及数字中的一种
+     * 是否包含
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isLetterOrDigit(String str) {
+        boolean isLetterOrDigit = false;//定义一个boolean值，用来表示是否包含字母或数字
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetterOrDigit(str.charAt(i))) {   //用char包装类中的判断数字的方法判断每一个字符
+                isLetterOrDigit = true;
+            }
+        }
+        String regex = "^[a-zA-Z0-9]+$";
+        boolean isRight = isLetterOrDigit && str.matches(regex);
+        return isRight;
+    }
+
+    /**
+     * 规则2：至少包含大小写字母及数字中的两种
+     * 是否包含
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isLetterDigit(String str) {
+        boolean isDigit = false;//定义一个boolean值，用来表示是否包含数字
+        boolean isLetter = false;//定义一个boolean值，用来表示是否包含字母
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {   //用char包装类中的判断数字的方法判断每一个字符
+                isDigit = true;
+            } else if (Character.isLetter(str.charAt(i))) {  //用char包装类中的判断字母的方法判断每一个字符
+                isLetter = true;
+            }
+        }
+        String regex = "^[a-zA-Z0-9]+$";
+        boolean isRight = isDigit && isLetter && str.matches(regex);
+        return isRight;
+    }
+
+    /**
+     * 规则3：必须同时包含大小写字母及数字
+     * 是否包含
+     *
+     * @param str
+     * @return
+     */
+    public static boolean isContainAll(String str) {
+        boolean isDigit = false;//定义一个boolean值，用来表示是否包含数字
+        boolean isLowerCase = false;//定义一个boolean值，用来表示是否包含字母
+        boolean isUpperCase = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {   //用char包装类中的判断数字的方法判断每一个字符
+                isDigit = true;
+            } else if (Character.isLowerCase(str.charAt(i))) {  //用char包装类中的判断字母的方法判断每一个字符
+                isLowerCase = true;
+            } else if (Character.isUpperCase(str.charAt(i))) {
+                isUpperCase = true;
+            }
+        }
+        String regex = "^[a-zA-Z0-9]+$";
+        boolean isRight = isDigit && isLowerCase && isUpperCase && str.matches(regex);
+        return isRight;
+    }
+
+    /**
+     *   判断EditText输入的数字、中文还是字母方法
+     */
+    public static void whatIsInput( EditText edInput) {
+        String txt = edInput.getText().toString();
+
+        Pattern p = Pattern.compile("[0-9]*");
+        Matcher m = p.matcher(txt);
+        if (m.matches()) {
+            ToastUtil.show("输入的是数字");
+
+        }
+        p = Pattern.compile("[a-zA-Z]");
+        m = p.matcher(txt);
+        if (m.matches()) {
+            ToastUtil.show("输入的是字母");
+
+        }
+        p = Pattern.compile("[\u4e00-\u9fa5]");
+        m = p.matcher(txt);
+        if (m.matches()) {
+            ToastUtil.show("输入的是汉字");
+        }
+    }
+}
---
+++ app/src/main/java/text/qiao/com/utils/utils/GetToken.java
@@ -0,0 +1,83 @@
+package text.qiao.com.utils.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.guangyao.friendschat.model.entity.login.Token;
+import com.guangyao.friendschat.utils.log.LogUtil;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Administrator on 2018/3/31.
+ */
+
+public class GetToken {
+
+    public static String GetRongCloudToken(String username) {
+        StringBuffer res = new StringBuffer();
+        String url ="https://api.cn.ronghub.com/user/getToken.json";
+        String App_Key = "vnroth0kv84io"; //开发者平台分配的 App Key。
+        String App_Secret ="bcTvPguQK8A8";
+        String Timestamp = String.valueOf(System.currentTimeMillis() / 1000);//时间戳，从 1970 年 1 月 1 日 0 点 0 分 0 秒开始到现在的秒数。
+        String Nonce = String.valueOf(Math.floor(Math.random() * 1000000));//随机数，无长度限制。
+        String Signature = sha1(App_Secret + Nonce + Timestamp);//数据签名。
+//        Logger.i(Signature);
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setHeader("App-Key", App_Key);
+        httpPost.setHeader("Timestamp", Timestamp);
+        httpPost.setHeader("Nonce", Nonce);
+        httpPost.setHeader("Signature", Signature);
+        httpPost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(1);
+        nameValuePair.add(new BasicNameValuePair("userId",username));
+        HttpResponse httpResponse = null;
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair,"utf-8"));
+            httpResponse = httpClient.execute(httpPost);
+            BufferedReader br = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                res.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LogUtil.e("qiao","结果：---"+res.toString());
+
+        Token userRespone = JSON.parseObject(res.toString(), Token.class);
+//        Logger.i(userRespone.getCode()+"");userRespone.getToken()
+        return null;
+    }
+    //SHA1加密//http://www.rongcloud.cn/docs/server.html#通用_API_接口签名规则
+    private static String sha1(String data){
+        StringBuffer buf = new StringBuffer();
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            md.update(data.getBytes());
+            byte[] bits = md.digest();
+            for(int i = 0 ; i < bits.length;i++){
+                int a = bits[i];
+                if(a<0) a+=256;
+                if(a<16) buf.append("0");
+                buf.append(Integer.toHexString(a));
+            }
+        }catch(Exception e){
+
+        }
+        return buf.toString();
+    }
+}
